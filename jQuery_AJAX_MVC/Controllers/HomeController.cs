0d2734a5d5d16b450e4cc307dfb591a0dbec7d3e
Vinod Kandanchathanpilli <vinodk@myyp.com>
2018-09-24 14:36:43 -0700

    first cc final

---
+++ jQuery_AJAX_MVC/Controllers/HomeController.cs
@@ -0,0 +1,67 @@
+﻿using jQuery_AJAX_MVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace jQuery_AJAX_MVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View("final1");
+        }
+
+        public ActionResult CC()
+        {
+            return View();
+        }
+        public ActionResult CC2()
+        {
+            return View();
+        }
+        public ActionResult CC3()
+        {
+            return View();
+
+        }
+
+        public ActionResult final()
+        {
+            return View();
+        }
+
+        public ActionResult final1()
+        {
+            return View();
+        }
+
+
+
+        [HttpPost]
+        public JsonResult AjaxMethod(string name)
+        {
+            PersonModel person = new PersonModel
+            {
+                Name = name,
+                DateTime = DateTime.Now.ToString()
+            };
+            return Json(person);
+        }
+
+        [HttpPost]
+        public JsonResult AjaxPostCall(Employee employeeData)
+        {
+            Employee employee = new Employee
+            {
+                CCToken = employeeData.CCToken,
+                ExpDate = employeeData.ExpDate,
+                CCV = employeeData.CCV
+            };
+            return Json(employee, JsonRequestBehavior.AllowGet);
+        }  
+    }
+}
---
+++ jQuery_AJAX_MVC/Models/Employee.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace jQuery_AJAX_MVC.Models
+{
+    public class Employee
+    {
+        public string CCToken
+        {
+            get;
+            set;
+        }
+        public string ExpDate
+        {
+            get;
+            set;
+        }
+        public string CCV
+        {
+            get;
+            set;
+        }
+    }  
+}
